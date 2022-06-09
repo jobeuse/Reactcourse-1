e1bb80be7f374d05075e16e0dc5f9504eaa98aee
Yobu Iradukunda <62539654+jobeuse@users.noreply.github.com>
2022-06-09 23:20:25 +0200

    creating custom component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./styles.css";
+import ExpenseItems from "./components/ExpenseItems";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <ExpenseItems />
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
